16a3bae64b2684022109466a43663cda86270285
Nahun Menem <86641118+NahunMenem@users.noreply.github.com>
2026-02-03 14:33:52 -0300

    Update mapa-medicos.tsx

--- app/dashboard/mapa-medicos.tsx
+++ app/dashboard/mapa-medicos.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import L from "leaflet";
-import type { LatLngTuple } from "leaflet";
 
 const API = process.env.NEXT_PUBLIC_API_BASE!;
 
